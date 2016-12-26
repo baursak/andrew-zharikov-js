27c45c8bf7a278b97bef2457b09839407d5570d0
baursak <baursak@gmail.com>
2016-12-26 11:29:54 +0300

    lesson1-video1b-00h24m14s : calculator

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-// пользовательская функция print_r()
-function print_r(obj) {
-    var s = '<h1>' + obj + '</h1>';
-    s += '<ol>';
+// калькулятор
+window.onload = function () {
+    var inpX = document.getElementById('inpX');
+    var inpY = document.getElementById('inpY');
+    var btn = document.getElementById('btnRun');
+    var spn = document.getElementById('spnResult');
+    btn.onclick = function () {
+        var x = inpX.value;
+        var y = inpY.value;
+        var res = parseFloat(x) + parseFloat(y);
+        spn.innerHTML = res;
+        btn.disabled = true;
+    };
+    inpX.onkeydown = function () {
+        btn.disabled = false;
+    };
+    inpY.onkeydown = function () {
+        btn.disabled = false;
+    };
+};
 
-    for (p in obj)
-        s += '<li><b>' + p + '</b> : ' + obj[p] + '</li>';
-
-    s += '</ol>';
-    window.document.body.innerHTML = s;
-}
-// вызов функции print_r()
-var divZero = document.body.children[0];
-var spanTwo = divZero.children[1];
-print_r(spanTwo);
